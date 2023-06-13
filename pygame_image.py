f4bea86b255ba597f0a14aac3e38fe69ac58dcd7
ymf65 <c0a221001d@edu.teu.ac.jp>
2023-06-13 17:05:37 +0900

    演習１

--- pygame_image.py
+++ pygame_image.py
@@ -1,5 +1,6 @@
 import sys
 import pygame as pg
+import math
 
 
 def main():
@@ -8,22 +9,30 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
 
-    # kou = pg.Surface((0, 0))
+    kou = pg.Surface((0, 0))
     kou = pg.image.load("ex01/fig/3.png")
     kou = pg.transform.flip(kou, True, False)
-    kklist = [pg.transform.rotozoom(kou, 10, 1)]
+    kou = pg.transform.rotozoom(kou, 10, 1)
+    lis = [kou]
 
     tmr = 0
+    x = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
-        screen.blit(kou, [300, 200])
+        screen.blit(bg_img, [0-x,0])
+        screen.blit(bg_img, [1600-x, 0])
+        if x < 800:
+            x += 1
+        elif x >= 1600:
+            x += 1
+        
+        screen.blit(kou, [300, 200+10*math.sin(math.radians(tmr))])
         
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(500)
 
 
 if __name__ == "__main__":
